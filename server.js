21451ddab6054384ecd7b2ac05b5437e3c0fa7ca
GitWebosta <git@webosta.net>
2023-07-16 22:46:15 +0200

    add projecto to repository

---
+++ server.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const { port } = require('./config');
+const bodyParser = require('body-parser');
+const mime = require('mime')
+const cors = require('cors')
+const puppeteer = require('puppeteer')
+
+app.use(express.static(path.join(__dirname, './')));
+
+// headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Dostosuj '*', aby zezwalać tylko na konkretne domeny
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+// disabled buffor
+app.disable('etag');
+
+// mime
+mime.define({
+  'application/json': ['json'],
+}, { force: true })
+
+// pasery
+// Content-type: application/json
+app.use(bodyParser.json())
+
+// fix cors
+app.use(cors())
+
+function scrollToBottom({
+  page,
+  distancePx,
+  speedMs,
+  scrollTimeoutMs,
+  eltToScroll,
+}) {
+  return page.evaluate(
+    (distancePx, speedMs, scrollTimeoutMs, eltToScroll) => {
+      return new Promise((resolve) => {
+        const elt = document.querySelector(eltToScroll);
+        let totalHeight = 0;
+        const timer = setInterval(() => {
+          const scrollHeight = elt.scrollHeight;
+          window.scrollBy(0, distancePx);
+          totalHeight += distancePx;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, speedMs);
+
+        setTimeout(() => {
+          clearInterval(timer);
+          resolve();
+        }, scrollTimeoutMs);
+      });
+    },
+    distancePx,
+    speedMs,
+    scrollTimeoutMs,
+    eltToScroll
+  );
+}
+ 
+// {
+//   headless: true,
+//   ignoreDefaultArgs: ['--disable-extensions'],
+//   args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//   protocolTimeout: 120000
+// }
+
+const scrollAndGetPageHTML = async (req, res) => {
+  // Set up Chromium browser and page.
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try{
+    const page = await browser.newPage();
+
+    //default timeout to navigation-related function(goBack(), goForward(), goto(), reload(), setContent(), waitForNavigation())
+    page.setDefaultNavigationTimeout(0);
+    // //default timeout tto navigation function and waiting function(waitFor(), waitForFunction(), waitForRequest(), waitForResponse(), waitForSelector(), waitForXPath())
+    page.setDefaultTimeout(0); 
+    // Navigate to the example page.
+    await page.goto(req.body.url, {waitUntil: 'networkidle2', timeout: 0});
+  
+    await scrollToBottom({
+      page,
+      distancePx: 200,
+      speedMs: 50,
+      scrollTimeoutMs: 10000,
+      eltToScroll: "body" 
+    })
+
+    const html = await page.content();
+  
+    await browser.close();
+    res.send(html);
+  }catch (error){
+
+  }
+  
+};
+
+app.post('/htmlCode', scrollAndGetPageHTML);
+
+
+// server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  
