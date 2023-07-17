22581710866f558ff11656ba64155db0b3689661
GitWebosta <git@webosta.net>
2023-07-17 11:31:41 +0200

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Dostosuj '*', aby zezwalać tylko na konkretne domeny
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader("Content-Type", "application/json;charset=utf-8");
+  res.setHeader("Content-Type", "text/html");
   next();
 });
 
@@ -23,12 +23,12 @@ app.disable('etag');
 
 // mime
 mime.define({
-  'application/json': ['json'],
+  'text/html': ['html'],
 }, { force: true })
 
 // pasery
 // Content-type: application/json
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
 // fix cors
 app.use(cors())
